f9c90c6f31b1600b9f266916e83ee0336a178e77
Kukieu <stiekerosiem@gmail.com>
2019-11-19 20:45:29 +0100

    Revert "node-oauth20-provider"
    This reverts commit 82dcf563a52bc29c740512167af66027095a8e10.

--- app/index.js
+++ app/index.js
@@ -2,11 +2,7 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var sql = require("mssql");
-var oauth2lib = require('oauth20-provider');
-var oauth2 = new oauth2lib({ log: { level: 2 } });
-
 var app = express();
-
 
 // Body Parser Middleware
 app.use(bodyParser.json());
@@ -26,25 +22,6 @@ var server = app.listen(process.env.PORT || 8080, function () {
     console.log("App now running on port", port);
 });
 
-app.use(oauth2.inject());
-app.post('/token', oauth2.controller.token);
-
-function isAuthorized(req, res, next) {
-    if (req.session.authorized) next();
-    else {
-        var params = req.query;
-        params.backUrl = req.path;
-        res.redirect('/login?' + query.stringify(params));
-    }
-};
-
-app.get('/authorization', isAuthorized, oauth2.controller.authorization, function (req, res) {
-    // Render our decision page
-    // Look into ./test/server for further information
-    res.render('authorization', { layout: false });
-});
-app.post('/authorization', isAuthorized, oauth2.controller.authorization);
-
 //Initiallising connection string
 var dbConfig = {
     user: "marcin.kmiecik",
@@ -60,28 +37,28 @@ var dbConfig = {
 // var dbConfig = "Data Source=yerbaland.database.windows.net;Encrypt=true;User Id=marcin.kmiecik;Password=axSIFux9@;Initial Catalog=DnD;Integrated Security=True";
 
 //Function to connect to database and execute query
-// var executeQuery = function (req, res, query) {
-//     sql.connect(dbConfig, function (err) {
-//         if (err) {
-//             console.log("Error while connecting database :- " + err);
-//             res.send(err);
-//         }
-//         else {
-//             // create Request object
-//             var request = new sql.Request();
-//             // query to the database
-//             request.query(query, function (err, res) {
-//                 if (err) {
-//                     console.log("Error while querying database :- " + err);
-//                     res.send(err);
-//                 } else {
-//                     // console.log(res);
-//                     res.json({ recordsets });
-//                 }
-//             });
-//         }
-//     });
-// }
+var executeQuery = function (req, res, query) {
+    sql.connect(dbConfig, function (err) {
+        if (err) {
+            console.log("Error while connecting database :- " + err);
+            res.send(err);
+        }
+        else {
+            // create Request object
+            var request = new sql.Request();
+            // query to the database
+            request.query(query, function (err, res) {
+                if (err) {
+                    console.log("Error while querying database :- " + err);
+                    res.send(err);
+                } else {
+                    // console.log(res);
+                    res.json({ recordsets });
+                }
+            });
+        }
+    });
+}
 
 // sql.connect(dbConfig, function (err) {
 //     if (err) throw err;
