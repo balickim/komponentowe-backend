4d53f8c8e8e1410d6e3b29ce33834a2e10c6d994
Michał Balicki <stiekerosiem@gmail.com>
2019-12-01 14:16:21 +0100

    added doctor api

---
+++ app/api/doctor.js
@@ -0,0 +1,112 @@
+const { Router } = require('express');
+const DoctorTable = require('../doctor/table');
+const { correctApiKey } = require('./helper')
+
+const router = new Router();
+
+router.get('/', (req, res, next) => {
+    const { apikey } = req.headers;
+    const { login } = req.body;
+
+    correctApiKey(apikey)
+        .then(() => {
+            return DoctorTable.getDoctor({ login })
+        })
+        .then((doctor) => { res.json(doctor) })
+        .catch(error => next(error));
+});
+
+router.post('/signup', (req, res, next) => {
+    const { apikey } = req.headers;
+    const {
+        login,
+        haslo } = req.body;
+
+    correctApiKey(apikey)
+        .then(() => {
+            DoctorTable.getDoctor({ login })
+                .then(({ doctor }) => {
+                    if (!doctor) {
+
+                        return DoctorTable.storeDoctor({ login, haslo })
+
+                    } else {
+                        const error = new Error('Ten login jest już zajęty!');
+
+                        error.statusCode = 409;
+
+                        throw error;
+                    }
+                })
+                .catch(error => next(error));
+        })
+        .then(() => res.json({ message: 'dodano' }))
+        .catch(error => next(error));
+});
+
+router.post('/uzupelnijdane', (req, res, next) => {
+    const { apikey } = req.headers;
+    const {
+        login,
+        haslo,
+        imie,
+        nazwisko,
+        idplci,
+        ulica,
+        numerlokalu,
+        miejscowosc,
+        kodpocztowy,
+        numertelefonu,
+        email } = req.body;
+
+    correctApiKey(apikey)
+        .then(() => {
+            DoctorTable.getDoctor({ login })
+                .then(({ doctor }) => {
+                    if (doctor) {
+
+                        return DoctorTable.fillDataOfDoctor({ imie, nazwisko, idplci, ulica, numerlokalu, miejscowosc, kodpocztowy, numertelefonu, email })
+                            && DoctorTable.fillIdOfDataOfDoctor({ login, haslo, imie, nazwisko, email })
+
+                    } else {
+                        const error = new Error('Nie ma takiego lekarza');
+
+                        error.statusCode = 409;
+
+                        throw error;
+                    }
+                })
+                .catch(error => next(error));
+        })
+        .then(() => res.json({ message: 'dodano' }))
+        .catch(error => next(error));
+});
+
+router.post('/login', (req, res, next) => {
+    const { apikey } = req.headers;
+    const { login, haslo } = req.body;
+
+    correctApiKey(apikey)
+        .then(() => {
+            DoctorTable.getDoctor({ login })
+                .then(({ doctor }) => {
+                    if (doctor && doctor.haslo === haslo) {
+                        // const { sessionId } = account;
+
+                        // return console.log("zalogowano");
+                    } else {
+                        const error = new Error('Incorrect username or password');
+
+                        error.statusCode = 409;
+
+                        throw error;
+                    }
+                })
+                .then(() => res.json({ message: 'zalogowano' }))
+                .catch(error => next(error));
+        })
+        // .then(() => res.json({ message: 'dodano użytkownika ' + req.body.username }))
+        .catch(error => next(error));
+});
+
+module.exports = router;
---
+++ app/doctor/table.js
@@ -0,0 +1,67 @@
+const pool = require('../../databasePool');
+
+class DoctorTable {
+    static storeDoctor({ login, haslo }) {
+        return new Promise((resolve, reject) => {
+            pool.query(
+                `INSERT INTO lekarz (login, haslo) VALUES ($1,$2);`,
+                [login, haslo],
+                (error, response) => {
+                    if (error) return reject(error);
+
+                    resolve();
+                }
+            );
+        });
+    }
+
+    static fillDataOfDoctor({ imie, nazwisko, idplci, ulica, numerlokalu, miejscowosc, kodpocztowy, numertelefonu, email }) {
+        return new Promise((resolve, reject) => {
+            pool.query(
+                `INSERT INTO danepracownika (imie, nazwisko, idplci, ulica, numerlokalu, miejscowosc, kodpocztowy, numertelefonu, email)
+                VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9);`,
+                [imie, nazwisko, idplci, ulica, numerlokalu, miejscowosc, kodpocztowy, numertelefonu, email],
+                (error, response) => {
+                    if (error) return reject(error);
+
+                    resolve();
+                }
+            );
+        });
+    }
+
+    static fillIdOfDataOfDoctor({ login, haslo, imie, nazwisko, email }) {
+        return new Promise((resolve, reject) => {
+            pool.query(
+                `UPDATE lekarz SET idpracownika = (SELECT idpracownika 
+                        FROM danepracownika 
+                        WHERE imie = $1 AND nazwisko = $2 AND email = $3
+                        LIMIT 1)
+                        WHERE login = $4 AND haslo = $5;`,
+                [imie, nazwisko, email, login, haslo],
+                (error, response) => {
+                    if (error) return reject(error);
+
+                    resolve();
+                }
+            );
+        });
+    }
+
+    static getDoctor({ login }) {
+        return new Promise((resolve, reject) => {
+            pool.query(
+                `SELECT idlekarza, login, haslo FROM lekarz 
+                WHERE login = $1`,
+                [login],
+                (error, response) => {
+                    if (error) return reject(error);
+
+                    resolve({ doctor: response.rows[0] });
+                }
+            )
+        });
+    }
+}
+
+module.exports = DoctorTable;
