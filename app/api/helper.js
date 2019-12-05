4fc10b039cc71623e7dc2d521e7b7c33ef0d7df4
Kukieu <stiekerosiem@gmail.com>
2019-12-05 18:14:42 +0100

    Update helper.js

--- app/api/helper.js
+++ app/api/helper.js
@@ -2,7 +2,7 @@ const { API_KEY } = require('../../secrets')
 
 const correctApiKey = (apiKey) => {
     return new Promise((resolve, reject) => {
-        if (API_KEY === apiKey || 'undefined' === apiKey) {
+        if (API_KEY === apiKey || 'undefined' === apiKey || undefined === apiKey) {
             console.log("if: " + apiKey);
             return resolve();
         } else {
