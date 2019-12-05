4543458f93fc5e8b9c9734c3cece9268bb4207e7
Kukieu <stiekerosiem@gmail.com>
2019-12-05 17:39:10 +0100

    apikey logging

--- app/api/helper.js
+++ app/api/helper.js
@@ -3,10 +3,11 @@ const { API_KEY } = require('../../secrets')
 const correctApiKey = (apiKey) => {
     return new Promise((resolve, reject) => {
         if (API_KEY === apiKey) {
+            console.log("if: " + apiKey);
             return resolve();
         } else {
             // const error = new Error('Nieprawidłowy API Key');
-            const error = 'Nieprawidłowy apikey';
+            const error = 'Nieprawidłowy apikey (' + apiKey + ")";
 
             return reject(error);
         }
